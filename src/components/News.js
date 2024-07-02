e956ae20f3536e8f2ae0c4d8d9b34ca2898a0068
sachinseengh <sachinseengh@gmail.com>
2024-07-02 17:52:29 +0545

    previous and next button added to change pages of news

--- src/components/News.js
+++ src/components/News.js
@@ -12,15 +12,51 @@ export default class News extends Component{
 
         this.state={
             articles:[],
+            page:1,
+            totalRes:0
 
         }
     }
 //it runs after render
    async componentDidMount(){
-    let url ="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd6881f708bd441bb1b87fa5a540c665";
+    let url ="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd6881f708bd441bb1b87fa5a540c665&page=1";
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({articles:parseData.articles});
+    this.setState({articles:parseData.articles,
+        totalRes:parseData.totalResults
+    });
+   }
+
+   Next=async()=>{
+    
+    if(this.state.page+1>Math.ceil(this.state.totalRes/20)){
+
+    }else{
+    let url =`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd6881f708bd441bb1b87fa5a540c665&page=${this.state.page+1}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState(
+        {articles:parseData.articles,
+            page :this.state.page+1
+        }
+        
+
+    );
+}
+
+   }
+   Prev=async()=>{
+      
+      let url =`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd6881f708bd441bb1b87fa5a540c665&page=${this.state.page - 1}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState(
+        {articles:parseData.articles,
+            page :this.state.page - 1
+        }
+        
+
+    );
    }
     render(){
         return(
@@ -31,13 +67,17 @@ export default class News extends Component{
                     <hr />
                     <div className="row">
                     {this.state.articles.map((element)=>{
-                        return <div className="col md-3 mb-5" key={element.url}>
+                        return <div className="col md-3 mb-2" key={element.url}>
                         <NewItem title={element.title?element.title.slice(0,60)+"....":" "} description={element.description?element.description.slice(0,80)+"...": " "} imgUrl={element.urlToImage} url={element.url}/>
                         </div>
                         
                     })}
                         </div>
-                        
+                    </div>
+
+                    <div className="container d-flex justify-content-between mb-5">
+                    <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.Prev}> &larr; Previous</button>
+                    <button disabled={this.state.page+1>Math.ceil(this.state.totalRes/20)}  type="button" class="btn btn-dark" onClick={this.Next}>Next &rarr;</button>
                     </div>
                 
              
